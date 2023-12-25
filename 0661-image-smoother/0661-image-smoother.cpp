d4c3b87d42c205aa930f2c84020fd8547140dffc
Pritish Dakhole <76050107+perceus32@users.noreply.github.com>
2023-12-25 16:16:29 +0530

    Time: 38 ms (83.45%), Space: 22.1 MB (64.06%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        //TC : O(M*N)  ; SC: O(1)
         //m = number of rows; n = number of columns
         int m = img.size(), n = img[0].size(), sum = 0, count = 0;
-        vector<vector<int>> res(m, vector<int>(n, 0));
+        //vector<vector<int>> res(m, vector<int>(n, 0));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 //narrowed down to a cell
@@ -11,16 +12,21 @@ public:
                     for(int l=-1; l<2; l++){
                         //cout<<i<<" "<<j<<" "<<i+k<<" "<<j+l<<endl;
                         if((i+k)>=0 && (i+k)<m && (j+l)>=0 && (j+l)<n){
-                            sum += img[i+k][j+l];
+                            sum += (img[i+k][j+l] & 0xFF);
                             count++;
                         }
                     }
                 }
-                res[i][j]=sum/count;
+                img[i][j] |= (sum/count << 8);
                 sum = 0;
                 count = 0;
             }
         }
-        return res;
+        for(int i=0 ;i<m; i++){
+            for(int j=0; j<n; j++){
+                img[i][j] >>= 8;
+            }
+        }
+        return img;
     }
 };
